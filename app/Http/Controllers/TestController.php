b618eb8e674d008eac64374f314f81908ce812b4
Alexander Schetnikov <hi.sanyaaa@ya.ru>
2019-12-02 19:02:54 +0700

    Добавлен класс TestOne и метод getOne для класса TestController (ДЗ 5.4)

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+class TestOne {
+    private $age = 22;
+    private $name = 'Alexander';
+    private $male = true;
+
+    public function getParams() {
+        $array = [];
+        foreach ($this as $key => $value) {
+            array_unshift($array, [$key => $value]);
+        }
+        return $array;
+    }
+
+    public function setAge($age) {
+        $this->age = $age;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setMale($male) {
+        $this->male = $male;
+    }
+}
+
 class TestController extends Controller
 {
-    //
+    public function getOne() {
+        $test = new TestOne();
+
+        dd($test->getParams());
+    }
 }
