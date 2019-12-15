93c457dc6b40816cdafd86a30757463da91a2fea
Alexander Schetnikov <go.sanyaaa@gmail.com>
2019-12-15 14:31:22 +0700

    Добавлена таблица tests (ДЗ 7.1)

---
+++ database/migrations/2019_12_15_065504_create_test_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('text');
+            $table->tinyInteger('is_enabled')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test');
+    }
+}
